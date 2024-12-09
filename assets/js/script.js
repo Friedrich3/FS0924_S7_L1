c7e150615cdb9cc2e6206ccbded3fec2d90fdacc
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-12-09 17:22:55 +0100

    tentativo double check

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,66 @@
 
 //ESERCIZIO UNO
-class User{
-    constructor(_firstName, _lastName, _age, _location){
+class User {
+    constructor(_firstName, _lastName, _age, _location) {
         this.firstName = _firstName;
         this.lastName = _lastName;
         this.age = _age;
         this.location = _location;
     }
 
-    compareAge(element){
-        if(this.age === element.age){
+    compareAge(element) {
+        if (this.age === element.age) {
             return console.log(`L'età di ${this.firstName} è uguale a quella di ${element.firstName} `);
-        }else if (this.age > element.age){
+        } else if (this.age > element.age) {
             return console.log(`${this.firstName} è più vecchio di ${element.firstName}`);
-        }else{
+        } else {
             return console.log(`${this.firstName} è più giovane di ${element.firstName}`);
         }
     }
 }
 
-const utente1 = new User("Mario","Rossi", "72", "Roma");
-const utente2 = new User("Paolo","Bianchi", "72", "Genova");
-const utente3 = new User("Marco","Neri", "54", "Genova");
+const utente1 = new User("Mario", "Rossi", "72", "Roma");
+const utente2 = new User("Paolo", "Bianchi", "72", "Genova");
+const utente3 = new User("Marco", "Neri", "54", "Genova");
 
 utente1.compareAge(utente2);    //ETA UGUALE
 utente2.compareAge(utente3);    //OBJ MAGGIORE
 utente3.compareAge(utente1);    //OBJ MINORE
 
 //ESERCIZIO 2
-const pets= [];
-const Owners =[];
+const pets = [];
+const Owners = [];
 const btnAdd = document.getElementById("btnAdd");
 
 
 class Pet {
-    constructor(_petName, _ownerName,_species,_breed){
+    constructor(_petName, _ownerName, _species, _breed) {
         this.petName = _petName;
         this.ownerName = _ownerName;
         this.species = _species;
         this.breed = _breed;
     }
 
-    checkOwner(){
-        if(Owners.includes(this.ownerName))
+    checkOwner() {
+        if (Owners.includes(this.ownerName)) {
             console.log("true");
-        else{
+            printOwner(true,this.ownerName,this.petName);
+        } else {
             console.log("false");
+            printOwner(false,this.ownerName,this.petName);
         }
     }
 }
 
 
-btnAdd.addEventListener("click", function(e){
+btnAdd.addEventListener("click", function (e) {
     e.preventDefault();
     const petName = document.getElementById("petName").value;
     const ownerName = document.getElementById("ownerName").value;
     const species = document.getElementById("petSpecies").value;
     const breed = document.getElementById("petBreed").value;
-    
-    const pet = new Pet(petName,ownerName,species,breed);
+
+    const pet = new Pet(petName, ownerName, species, breed);
     pets.push(pet);
     pet.checkOwner();
     Owners.push(ownerName);
@@ -67,7 +69,7 @@ btnAdd.addEventListener("click", function(e){
     console.log(Owners);
 });
 
-function addPet(){
+function addPet() {
     let animalList = document.getElementById("animalList");
     let formAnimals = document.getElementById("formAnimals");
     animalList.innerHTML = "";
@@ -80,4 +82,19 @@ function addPet(){
     formAnimals.reset();
 }
 
+function printOwner(booleano,ownerName,petName) {
+    let ownerList = document.getElementById("Owners");
+    
+    if (booleano) {
+        let listItem = document.getElementById(`${ownerName}`);
+        listItem = listItem + `,${petName}`;
+    } else {
+        let newLi = document.createElement("li"); 
+        newLi.setAttribute("id",`${ownerName}`);
+        newLi.innerText = `${ownerName} owns : ${petName}`;
+        ownerList.appendChild(newLi);
+        
+    }
+
+}
 
